885c8bd97e9e8fb8b27dbf1662780bb15fcf3289
Levid0 <89023126+Levid0@users.noreply.github.com>
2023-04-18 15:49:48 +0000

    introductory scrolling timeline

--- timeline-project/timeline/src/pages/Timeline.js
+++ timeline-project/timeline/src/pages/Timeline.js
@@ -1,5 +1,67 @@
-const Blogs = () => {
-    return <h1>Blog Articles</h1>;
-  };
+import React from 'react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import 'react-horizontal-scrolling-menu/dist/styles.css';
+
+const getItems = () =>
+  Array(20)
+    .fill(0)
+    .map((_, ind) => ({ id: `element-${ind}` }));
+
+
+const Timeline = () => {
+  const [items, setItems] = React.useState(getItems);
+  const [selected, setSelected] = React.useState([]);
+  const [position, setPosition] = React.useState(0);
+
+  const isItemSelected = (id) => !!selected.find((el) => el === id);
+
+  const handleClick =
+    (id) =>
+    ({ getItemById, scrollToItem }) => {
+      const itemSelected = isItemSelected(id);
+
+      setSelected((currentSelected) =>
+        itemSelected
+          ? currentSelected.filter((el) => el !== id)
+          : currentSelected.concat(id)
+      );
+    };
+
+    return <ScrollMenu >
+    {items.map(({ id }) => (
+      <Card
+        itemId={id} // NOTE: itemId is required for track items
+        title={id}
+        key={id}
+        onClick={handleClick(id)}
+        selected={isItemSelected(id)}
+      />
+    ))}
+  </ScrollMenu>
+
+function Card({ onClick, selected, title, itemId }) {
+const visibility = React.useContext(VisibilityContext);
+
+return (
+  <div
+    onClick={() => onClick(visibility)}
+    style={{
+      width: '160px',
+    }}
+    tabIndex={0}
+  >
+    <div className="card">
+      <div>{title}</div>
+      <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
+      <div>selected: {JSON.stringify(!!selected)}</div>
+    </div>
+    <div
+      style={{
+        height: '200px',
+      }}
+    />
+  </div>
+);
+}}
   
-  export default Blogs;
+  export default Timeline;
