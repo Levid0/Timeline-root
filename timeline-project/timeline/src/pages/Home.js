969667db6997fdd37deec3d5b2fbdf79b1b794de
Levid0 <89023126+Levid0@users.noreply.github.com>
2023-04-18 13:00:48 +0000

    Initial commit

---
+++ timeline-project/timeline/src/pages/Home.js
@@ -0,0 +1,15 @@
+import logo from "../images/logo.png"
+import { useNavigate } from 'react-router-dom';
+const Home = () => {
+    const navigate = useNavigate();
+    function navigateTimeline(){
+        navigate("/timeline")
+    }
+    return <div class="flex flex-vertical flex-centre centre flex-gap" onClick={navigateTimeline}>
+        <div class="title">Immigration Detention Timeline</div>
+        <div style={{fontSize: "150%"}}>with</div>
+        <img src={logo} alt="logo" class="logo-intro"></img>
+    </div>;
+  };
+  
+  export default Home;
