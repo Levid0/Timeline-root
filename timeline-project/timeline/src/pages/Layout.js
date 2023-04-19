dbd1779356aa4e5d555427b9118d861e4831dc2a
Levid0 <89023126+Levid0@users.noreply.github.com>
2023-04-19 00:52:07 +0000

    getting somewhere

--- timeline-project/timeline/src/pages/Layout.js
+++ timeline-project/timeline/src/pages/Layout.js
@@ -8,12 +8,6 @@ const Layout = () => {
           <li>
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
         </ul>
       </nav>
 
--- timeline-project/timeline/src/pages/Timeline.js
+++ timeline-project/timeline/src/pages/Timeline.js
@@ -2,13 +2,17 @@ import React from 'react';
 import "../input.css"
 const Timeline = () => {
   
-    return <div id="container" class="flex flex-col my-24">
-    <h1 class="text-3xl text-gray-900 text-center mb-4">Our slider's title</h1>
-    <div id="scrolling-content" class="flex overflow-x-scroll snap-x">
-      <div class="w-96 h-64 px-4 flex-shrink-0 snap-center">
-        <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" alt="" />
+    return <div id="scrolling-content" class="flex overflow-x-scroll snap-x">
+      <div class="flex-shrink-0 card snap-center flex flex-col justify-center text-center">
+        <div>Population and cost and date</div><div class="align-middle bg-black timeline"><div class="circle absolute">Circle</div></div><div>Clickable thinggggggggggggggg fhjd d d d fsjoj sdfj lsfjlv sfv sj dil snmdlvknsdv jlnj sdg</div>
       </div>
-      <div class="w-96 h-64 px-4 flex-shrink-0 snap-center">
+      <div class="flex-shrink-0 card snap-center flex flex-col justify-center text-center">
+        <div>Population and cost and date</div><div class="align-middle bg-black timeline"><div class="circle absolute">Circle</div></div><div>Clickable thinggggggggggggggg fhjd d d d fsjoj sdfj lsfjlv sfv sj dil snmdlvknsdv jlnj sdg</div>
+      </div>
+      <div class="flex-shrink-0 card snap-center flex flex-col justify-center text-center">
+        <div>Population and cost and date</div><div class="align-middle bg-black timeline"><div class="circle absolute">Circle</div></div><div>Clickable thinggggggggggggggg fhjd d d d fsjoj sdfj lsfjlv sfv sj dil snmdlvknsdv jlnj sdg</div>
+      </div>
+      <div class="card px-10 flex-shrink-0 snap-center">
         <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" alt=""/>
       </div>
       <div class="w-96 h-64 px-4 flex-shrink-0 snap-center">
@@ -24,7 +28,6 @@ const Timeline = () => {
         <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" alt=""/>
       </div>
     </div>
-  </div>
   };
   
   export default Timeline;
