3d8d7c707ab39d25f07a37dd6cf99448750061b2
Levid0 <89023126+Levid0@users.noreply.github.com>
2023-04-21 18:07:55 +0000

    scroll-snap

--- timeline-project/timeline/src/pages/Timeline.js
+++ timeline-project/timeline/src/pages/Timeline.js
@@ -2,13 +2,13 @@ import React from 'react';
 import "../input.css"
 const Timeline = () => {
   
-    return <div class="timeline-scroller">
+    return <div class="timeline-scroller snaps-inline">
       <div class="timeline-group">
         <div>Population and cost and date</div>
           <div class="">
             <div class="">Circle</div>
           </div>
-          <div>Clickable thinggggggggggggggg fhjd d d d fsjoj sdfj lsfjlv sfv sj dil snmdlvknsdv jlnj sdg</div>
+          <div>Clickable thinggggg fhjd d d d fsjoj sdfj lsfjlv sfv sj dil snmdlvknsdv jlnj sdg</div>
       </div>
       <div class="timeline-group">
         <div>Population and cost and date</div>
