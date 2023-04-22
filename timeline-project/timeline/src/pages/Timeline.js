2c6c2a9d00c05630a1f2fd8a75eb792699e128f3
Levid0 <89023126+Levid0@users.noreply.github.com>
2023-04-22 19:30:00 +0000

    back to grid babyyyy

--- timeline-project/timeline/src/pages/Timeline.js
+++ timeline-project/timeline/src/pages/Timeline.js
@@ -5,71 +5,16 @@ const Timeline = () => {
     return <div>
       <img src={logo} alt="logo" class="absolute top-right"></img>
     <div class="timeline-scroller snaps-inline ">
-      <div class="timeline-group flex flex-centre flex-vertical">
+      <div class="timeline-group ">
         <div class="timeline-top">Population and cost and date</div>
-        <div class="timeline-second"></div>
         <div class="line-group flex flex-centre line-group-start ">
           <div class="circle-group"></div>
         </div>
-        <div class="timeline-fourth"></div>
+        
         <div class="timeline-bottom">Clickable thinggggg fhjd il snmdlvknsdv jlnj sdg</div>
+        
       </div>
-      <div class="timeline-group flex flex-centre flex-vertical">
-        <div class="timeline-top">Population and cost and date</div>
-        <div class="timeline-second"></div>
-        <div class="line-group flex flex-centre ">
-          <div class="circle-group"></div>
-        </div>
-        <div class="timeline-fourth"></div>
-        <div class="timeline-bottom">Clickable thinggggg fhjd il snmdlvknsdv jlnj sdg</div>
       </div>
-      <div class="timeline-group flex flex-centre flex-vertical">
-        <div class="timeline-top">Population and cost and date</div>
-        <div class="timeline-second"></div>
-        <div class="line-group flex flex-centre ">
-          <div class="circle-group"></div>
-        </div>
-        <div class="timeline-fourth"></div>
-        <div class="timeline-bottom">Clickable thinggggg fhjd il snmdlvknsdv jlnj sdg</div>
-      </div>
-      <div class="timeline-group flex flex-centre flex-vertical">
-        <div class="timeline-top">Population and cost and date</div>
-        <div class="timeline-second"></div>
-        <div class="line-group flex flex-centre ">
-          <div class="circle-group"></div>
-        </div>
-        <div class="timeline-fourth"></div>
-        <div class="timeline-bottom">Clickable thinggggg fhjd il snmdlvknsdv jlnj sdg</div>
-      </div>
-      <div class="timeline-group flex flex-centre flex-vertical">
-        <div class="timeline-top">Population and cost and date</div>
-        <div class="timeline-second"></div>
-        <div class="line-group flex flex-centre ">
-          <div class="circle-group"></div>
-        </div>
-        <div class="timeline-fourth"></div>
-        <div class="timeline-bottom">Clickable thinggggg fhjd il snmdlvknsdv jlnj sdg</div>
-      </div>
-      <div class="timeline-group flex flex-centre flex-vertical">
-        <div class="timeline-top">Population and cost and date</div>
-        <div class="timeline-second"></div>
-        <div class="line-group flex flex-centre ">
-          <div class="circle-group"></div>
-        </div>
-        <div class="timeline-fourth"></div>
-        <div class="timeline-bottom">Clickable thinggggg fhjd il snmdlvknsdv jlnj sdg</div>
-      </div>
-      <div class="timeline-group flex flex-centre flex-vertical">
-        <div class="timeline-top">Population and cost and date</div>
-        <div class="timeline-second"></div>
-        <div class="line-group flex flex-centre ">
-          <div class="circle-group"></div>
-        </div>
-        <div class="timeline-fourth"></div>
-        <div class="timeline-bottom">Clickable thinggggg fhjd il snmdlvknsdv jlnj sdg</div>
-      </div>
-      
-    </div>
     </div>
   };
   
