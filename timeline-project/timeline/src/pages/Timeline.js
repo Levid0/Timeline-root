40d2ad89d5e65d1af09265209a9b00d151ef5761
Levid0 <89023126+Levid0@users.noreply.github.com>
2023-04-24 21:20:49 +0000

    fixed start spacing issue

--- timeline-project/timeline/src/pages/Timeline.js
+++ timeline-project/timeline/src/pages/Timeline.js
@@ -5,6 +5,7 @@ const Timeline = () => {
     return <div>
       <img src={logo} alt="logo" class="absolute top-right"></img>
     <div class="timeline-scroller snaps-inline ">
+      <div class="padding-left"></div>
       <div class="timeline-group timeline-group-start ">
         <div class="timeline-top">Population and cost and date</div>
         <div class="line-group flex flex-centre line-group-start ">
