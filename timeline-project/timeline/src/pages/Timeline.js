03bbd14ccbd42216564e60dec62b12a0387e19f5
Levid0 <89023126+Levid0@users.noreply.github.com>
2023-04-24 21:13:30 +0000

    spacing issue contained?

--- timeline-project/timeline/src/pages/Timeline.js
+++ timeline-project/timeline/src/pages/Timeline.js
@@ -5,7 +5,7 @@ const Timeline = () => {
     return <div>
       <img src={logo} alt="logo" class="absolute top-right"></img>
     <div class="timeline-scroller snaps-inline ">
-      <div class="timeline-group ">
+      <div class="timeline-group timeline-group-start ">
         <div class="timeline-top">Population and cost and date</div>
         <div class="line-group flex flex-centre line-group-start ">
           <div class="circle-group"></div>
