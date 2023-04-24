390fb6e265407ae404662c34042725a3d0c7c1db
Levid0 <89023126+Levid0@users.noreply.github.com>
2023-04-24 21:49:40 +0000

    beginning of great things

--- timeline-project/timeline/src/pages/Timeline.js
+++ timeline-project/timeline/src/pages/Timeline.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import "../input.css"
 import logo from "../images/logo.png"
+import dataPreviews from "./data-previews"
 const Timeline = () => {
     return <div>
       <img src={logo} alt="logo" class="absolute top-right"></img>
     <div class="timeline-scroller snaps-inline ">
       <div class="padding-left"></div>
-      <div class="timeline-group timeline-group-start ">
-        <div class="timeline-top">Population and cost and date</div>
-        <div class="line-group flex flex-centre line-group-start ">
-          <div class="circle-group"></div>
+      
+      {dataPreviews.map((card)=>{
+       return <div class="timeline-group ">
+        <div class="timeline-top">Population:{card.population} <br></br> Cost:{card.cost}</div>
+        <div class={`line-group flex flex-centre ${card.id===0?"line-group-start":""}`}>
+          <div class="circle-group">{card.year}</div>
         </div>
-        
-        <div class="timeline-bottom">Clickable thinggggg fhjd il snmdlvknsdv jlnj sdg</div>
-        
+        <div class="timeline-bottom">{card.Title}</div>
       </div>
-      <div class="timeline-group ">
-        <div class="timeline-top">Population and cost and date</div>
-        <div class="line-group flex flex-centre ">
-          <div class="circle-group"></div>
-        </div>
-        <div class="timeline-bottom">Clickable thinggggg fhjd il snmdlvknsdv jlnj sdg</div>
-      </div>
+      })}
+      
       <div class="timeline-group ">
         <div class="timeline-top">Population and cost and date</div>
         <div class="line-group flex flex-centre ">
---
+++ timeline-project/timeline/src/pages/data-previews.js
@@ -0,0 +1,48 @@
+const previews=[
+{
+    id:0,
+    population:2000,
+    cost:1000,
+    year:1872,
+    Title:"It all begins"
+},
+{
+    id:1,
+    population:2000,
+    cost:1000,
+    year:1872,
+    Title:"It all begins"
+},
+{
+    id:1,
+    population:2000,
+    cost:1000,
+    year:1872,
+    Title:"It all begins"
+},
+{
+    id:1,
+    population:2000,
+    cost:1000,
+    year:1872,
+    Title:"It all begins"
+},
+{
+    id:1,
+    population:2000,
+    cost:1000,
+    year:1872,
+    Title:"It all begins"
+},
+{
+    id:1,
+    population:2000,
+    cost:1000,
+    year:1872,
+    Title:"It all begins"
+},
+
+
+]
+
+export default previews
