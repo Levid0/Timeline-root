621ac016d1b2886828e2de4e841c7b83f11734a9
Levid0 <89023126+Levid0@users.noreply.github.com>
2023-04-25 01:53:10 +0000

    core functionality ;)

--- timeline-project/timeline/src/pages/displayedCard.js
+++ timeline-project/timeline/src/pages/displayedCard.js
@@ -1,9 +1,15 @@
 import { useParams } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
+import "../styles/dataClicked.css"
 function Displayed() {
   const { id } = useParams();
+  const navigate = useNavigate();
+  function handleClick(){
+        navigate(`/timeline`)
+    }
   return (
     <div>
+      <div className="returnButton" onClick={handleClick}>Return (placeholder for icon)</div>
       <div className="clk-title">${id}</div>
     </div>
   );
